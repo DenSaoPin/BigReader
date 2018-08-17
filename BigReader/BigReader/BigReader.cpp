ed9df389e5aec90401b592957fb931c69333c80b
DenSaoPin <densaopin90@gmail.com>
2018-08-17 12:29:26 +0300

    Good State

--- BigReader/BigReader/BigReader.cpp
+++ BigReader/BigReader/BigReader.cpp
@@ -2,66 +2,98 @@
 //
 
 #include "stdafx.h"
-#include <fstream>
-#include <iostream>
-#include <map>
-#include <unordered_map>
+
 //#define NULL = 0
-#define BUFF_SIZE 4085*8
+#define BUFF_SIZE 4096*1024
 //#define OFFSET 97;
 
-class FileReader
+namespace fs = std::experimental::filesystem;
+
+class Collector
 {
-	//FILE * pFile = nullptr;
-	//std::ifstream ifs;
-
 public:
+	std::list<fs::path> BooksList;
 
-	
-
-	//bool open(const char *file)
-	//{
-	//	pFile = fopen(file, "r");
-	//	
-	//	if (pFile == nullptr)
-	//		return false;
-	//	//ifs = std::ifstream(file);
-	//	return true;
-	//}
-
-	//size_t read(void *pBuff, int count)
-	//{
-	//	
-	//	//ifs.read((char *)pBuff, count);
-	//	//return ifs.gcount();
-	//	return ;
-	//}
-
-
-	//char GetChar()
-	//{
-	//	pFile = fopen(file, "r");
-
-	//	if (pFile == nullptr)
-	//		return false;
-
-	//	if (counter == readedSymbols)
-	//	{
-	//		readedSymbols = fread(buff, 1, BUFF_SIZE, pFile);
-
-	//		if (readedSymbols == 0)
-	//			return 0x00;
-
-	//		counter = 0;
-	//	}
-	//			
-	//	return buff[counter++];
-	//};
+	void CollectBooksRecursively(std::string patch)
+	{
+		fs::path Patch = patch;
+		for (auto & p : fs::recursive_directory_iterator(Patch))
+		{
+			if(p.path().extension() == ".txt")
+			{
+				BooksList.push_back(p.path());
+			}
+		}
+	}
 };
 
+//class Collector
+//{
+//public:
+//	std::vector<fs::path> BooksList;
+//
+//	void CollectBooksRecursively(LPCTSTR lpFolder)
+//	{
+//		TCHAR szFullPattern[MAX_PATH];
+//
+//		PathCombine(szFullPattern, lpFolder, _T("*"));
+//
+//		WIN32_FIND_DATA findFileData;
+//		HANDLE handle = FindFirstFile(szFullPattern, &findFileData);
+//
+//		if(handle != INVALID_HANDLE_VALUE)
+//		{
+//			do
+//			{
+//				PathCombine(szFullPattern, lpFolder, findFileData.cFileName);
+//
+//				if (findFileData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)
+//				{
+//					CollectBooksRecursively(szFullPattern);
+//				}
+//				if (findFileData.cFileName == L"*.txt")
+//				{
+//					BooksList.push_back(szFullPattern);
+//				}
+//			} while (FindNextFile(handle, &findFileData));
+//
+//			FindClose(handle);
+//		}
+//	}
+//};
 
+
+static char st_tolmap[127];
+void init()
+{
+	for(int i = 'A'; i < 'Z'; i++)
+	{
+		st_tolmap[i] = tolower(i);
+	}
+	for (int i = 'a'; i < 'z'; i++)
+	{
+		st_tolmap[i] = i;
+	}
+}
+inline char tol(char ch)
+{
+	return st_tolmap[ch];
+
+}
+inline bool alpha(unsigned char ch)
+{
+	return  !(ch < 65 || (ch > 91 && ch < 97) || ch > 122);
+}
 int main(int argc, char* argv[])
 {
+	Collector* collector = new Collector;
+	//const wchar_t* path  = L"//vmware-host/Shared Folders/Gut/aleph.gutenberg.org/";
+	//const wchar_t* fileMask;
+	std::string path  = "//vmware-host/Shared Folders/Gut/aleph.gutenberg.org/";
+
+	collector->CollectBooksRecursively(path);
+
+	init();
 	const int OFFSET = 97;
 	
 	std::unordered_map<std::string, int> ::iterator it;
@@ -71,7 +103,7 @@ int main(int argc, char* argv[])
 	size_t readedSymbols = 0;
 	size_t counter = 0;
 
-	unsigned char buff[BUFF_SIZE] = { 0 };
+	unsigned char *buff = new unsigned char[BUFF_SIZE];
 
 	FILE *pFile = fopen("../BigReader/data/bb.txt", "r");
 
@@ -80,15 +112,12 @@ int main(int argc, char* argv[])
 		std::cout << "Can't open file\n";
 		return -1;
 	}
-	
-	
 
 
 	std::string rwS;
 	char resultWord[255];
 	int rwC = 0;
 	std::unordered_map<std::string, int> ::iterator it_end = wordHashMap.end();
-	char ch;
 	while(true)
 	{
 		//(ch = fr.GetChar()) != 0
@@ -97,14 +126,18 @@ int main(int argc, char* argv[])
 			readedSymbols = fread(buff, 1, BUFF_SIZE, pFile);
 
 			if (readedSymbols == 0)
-				return 0;
+				break;
 
 			counter = 0;
 		}
 
-		buff[counter++];
+		char symbol = buff[counter++];
 
-		if ((unsigned char)buff[counter] > 127 || !isalpha(buff[counter]))
+
+		//if ((unsigned char)symbol > 127 /*|| !isalpha(symbol)*/)
+		unsigned char ch = (unsigned char)symbol;
+		//if (ch < 65 || (ch > 91 && ch < 97) || ch > 122) 
+		if(!alpha(symbol))
 		{
 			if(rwC == 0)
 			{
@@ -128,9 +161,13 @@ int main(int argc, char* argv[])
 			continue;
 		}
 		//TODO: Maybe use char array?
-		resultWord[rwC++] = tolower(buff[counter]);
-
+		resultWord[rwC++] = st_tolmap[symbol];
 	}
+	printf("%d", (int)wordHashMap.size());
 }
+
+
+
+
 
 
